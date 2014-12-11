218a996b4c4a3f5edf957bdd5511df59d0780176
aqmebrak <aqmebrak@gmail.com>
2014-12-11 14:49:47 +0100

    function displayWord

--- src/src/MotBienParenthese.java
+++ src/src/MotBienParenthese.java
@@ -1,7 +1,8 @@
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 /**
- * Created by user on 10/12/2014.
+ * Created by Jeremy and Matthieu on 10/12/2014.
  */
 public class MotBienParenthese {
 
@@ -15,6 +16,9 @@ public class MotBienParenthese {
 
     }*/
 
+    /*
+    * @authors: Matthieu | Jeremy
+    */
     public static boolean verifMotBP(String mot) {
         if (mot.charAt(0) == ')') {
             System.out.println("Ce mot n'est pas bien parenthésé !\n");
@@ -30,8 +34,7 @@ public class MotBienParenthese {
         if (compteur == 0) {
             System.out.println("Mot bien parenthésé !\n");
             return true;
-        }
-        else {
+        } else {
             System.out.println("Ce mot n'est pas bien parenthésé !\n");
             return false;
         }
@@ -41,12 +44,57 @@ public class MotBienParenthese {
         if (!verifMotBP(mot)) System.exit(0);
         int altitude = 0, max = 0;
 
-        for (int i=0; i<mot.length(); i++) {
+        for (int i = 0; i < mot.length(); i++) {
             if (mot.charAt(i) == '(') altitude++;
             else if (mot.charAt(i) == ')') altitude--;
             if (altitude > max) max = altitude;
         }
 
-        System.out.println("Profondeur de parenthèses : "+max+'\n');
+        System.out.println("Profondeur de parenthèses : " + max + '\n');
+    }
+
+    public static void displayWord(String w) {
+
+        if (verifMotBP(w)) {
+            System.out.println("Ce mot n'est pas bien parenthésé !");
+            return;
+        }
+
+        String dec = "\t";
+        String xdec = "";
+        int cpt = 0;
+
+        PrintStream p = System.out;
+        //for (int i = 0; i < w.length(); i++) {
+        //    p.print(w.charAt(i));
+        //}
+        p.println();
+
+        for (int i = 0; i != w.length(); i++) {
+            // p.print(w.charAt(i));
+            // p.print("i: "+ i + "cpt : " + cpt);
+            if (w.charAt(i) == ')') {
+
+                xdec = repeat(dec, cpt - 1);
+                p.print(xdec);
+                cpt--;
+            } else {
+
+                xdec = repeat(dec, cpt);
+                p.print(xdec);
+                cpt++;
+            }
+            p.println(w.charAt(i));
+
+        }
+    }
+
+    public static String repeat(String s, int n) {
+        //System.out.println(n);
+        if (n == 0) return "";
+        for (int i = 0; i < n; i++) {
+            s += s;
+        }
+        return s;
     }
 }
