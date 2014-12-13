cb276b348e5aafcdab1c499422ab2194a8e127f0
aqmebrak <aqmebrak@gmail.com>
2014-12-13 19:32:13 +0100

    j'ai refais l'algo de KAWAZAKI d'apres la photo qu'on a pris du tableau en amphi, j'ai tout laissé en long parce qu'on sait pas encore construire le GrandNombre, on aura plus qu'à modifier tout ca plus tard !

--- src/src/GrandEntier.java
+++ src/src/GrandEntier.java
@@ -13,20 +13,51 @@ public class GrandEntier {
     public GrandEntier(int numBits, Random rnd) throws TooBigNumberException {
 
         try {
-            if(!(numBits <= MAXBITLENGTH)) throw new TooBigNumberException();
+            if (!(numBits <= MAXBITLENGTH)) throw new TooBigNumberException();
+        } catch (Exception e) {
+            System.out.println("La longueur de votre nombre dépasse la longueur autorisée de 10 000 000");
         }
-        catch (Exception e) { System.out.println("La longueur de votre nombre dépasse la longueur autorisée de 10 000 000"); }
 
         rnd = new Random();
         //bigInt = rnd.nextInt(Math.pow(2,numBits));
     }
 
-    public GrandEntier(ArrayList<Integer> ge){
+    public GrandEntier(ArrayList<Integer> ge) {
 
     }
+    public static int getSize(long num) {
+        int ctr = 0;
+        while (num != 0) {
+            ctr++;
+            num /= 10;
+        }
+        return ctr;
+    }
 
-    public static GrandEntier multiplyFast(GrandEntier ge){
+    public static GrandEntier multiplyFast(/*GrandEntier ge*/) {
+        long x = 385;
+        long y = 123;
 
+        int N = (int) Math.max(getSize(x),getSize(y));
+
+        N = N / 2 + N % 2;
+        int m = (int) Math.pow(B, N);
+
+        long b = x / m;
+        long a = x - (b * m);
+
+        long d = y / m;
+        long c = y - (d * m);
+
+        long z0 = a * c;
+        long z1 = (a + b) * (c + d);
+        long z2 = b * d;
+
+        long result = (long) ( z0 + ((z1 - z0 - z2) * m) + (z2 * Math.pow(B, 2 * N)));
+
+        System.out.println("result ! " +result);
+
+        return null;
     }
 
     @Override
@@ -39,24 +70,27 @@ public class GrandEntier {
         return super.equals(obj);
     }
 
-    public boolean compareTo(GrandEntier e){
+    public boolean compareTo(GrandEntier e) {
 
+        return false;
     }
 
-    public static int lenght(){
+    public static int lenght() {
 
+        return 0;
     }
 
-    public static GrandEntier shiftLeft(int n){
+    public static GrandEntier shiftLeft(int n) {
 
+        return null;
     }
 
-    public static GrandEntier add(GrandEntier ge){
-
+    public static GrandEntier add(GrandEntier ge) {
+        return null;
     }
 
-    public static multiply(GrandEntier ge){
-
+    public static GrandEntier multiply(GrandEntier ge) {
+        return null;
     }
 }
 
