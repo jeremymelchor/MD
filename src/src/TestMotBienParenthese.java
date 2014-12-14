7c83d72c5005f24ddafb6d20c20384e6bc35b4cb
Melchor Jérémy <patchimou14@gmail.com>
2014-12-14 23:14:37 +0100

    Merge branch 'master' of C:\Users\user\IdeaProjects\MD with conflicts.

--- src/src/TestMotBienParenthese.java
+++ src/src/TestMotBienParenthese.java
@@ -6,6 +6,9 @@ public class TestMotBienParenthese {
     public static void main(String args[]) {
 
         //MotBienParenthese.verifMotBP("()))");
-        MotBienParenthese.profondeurParenthese("((()))");
+        //MotBienParenthese.profondeurParenthese("((()))");
+
+        GrandEntier a = new GrandEntier();
+        a.afficher();
     }
 }
