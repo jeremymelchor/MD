66ce5660fcd56bc0d3a89c65398ff326a8d5daf5
Melchor Jérémy <patchimou14@gmail.com>
2015-01-07 18:34:39 +0100

    TOUT MotBienParenthese FINI SA MERE LA PUTE

--- src/src/MotBienParenthese.java
+++ src/src/MotBienParenthese.java
@@ -10,9 +10,16 @@ public class MotBienParenthese {
     private static ArrayList<String> listeFinale = new ArrayList<String>();
     private static int Nfinal;
 
+    /**
+     * Question 1 Affiche tous les mots bien parenthésés de longueur 2n
+     *
+     * @param n la moitié de la longueur voulue
+     * @return une ArrayList contenant tous les mots bien parenthésés de
+     * longueur <= 2n
+     */
     public static ArrayList<String> enumMotsBP(int n) {
 
-       if (Thread.currentThread().getStackTrace()[2].getMethodName().equals("main")) {
+        if (Thread.currentThread().getStackTrace()[2].getMethodName().equals("main")) {
             Nfinal = n;
         }
 
@@ -37,20 +44,75 @@ public class MotBienParenthese {
         }
     }
 
-
+    /**
+     * Question 3 Calcule le nombre de Catalan récursivement
+     *
+     * @param n La moitié de la longueur du mot choisi
+     * @return Le nombre de mots bien parenthésés de longueur 2n
+     */
     public static int catalan(int n) {
-        final int i = n;
-
-        if (n <= 1) return 1;
+        int c = 0;
+        if (n == 0) return 1;
         else {
-            return ( ((i+n)/n) * catalan(n - 1) );
+            for (int i = 0; i < n; i++) c += catalan(i) * catalan(n - i - 1);
+        }
+
+        return c;
+    }
+
+    /**
+     * Question 5 Calcule le nombre de Catalan itérativement
+     *
+     * @param n la moitié de la longueur voulue
+     * @return le nombre de mots bien parenthésés de longueur 2n
+     */
+    public static long catalan2(int n) {
+
+        long stockage[] = new long[n + 1];
+        long res = 0;
+        stockage[0] = 1;
+        stockage[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            res = 0;
+            for (int j = 0; j < i; j++) res += (stockage[j] * stockage[i - 1 - j]);
+            stockage[i] = res;
+        }
+
+        return stockage[n];
+    }
+
+    /**
+     * Question 6 Compare le temps d'execution entre catalan1 et catalan2
+     *
+     * @param n la moitié de la longueur voulue
+     */
+    public static void testCatalan2(int n) {
+        long catalani;
+        long t0, t1;
+        long temps1, temps2;
+        for (int i = 10; i <= n; i++) {
+            t0 = System.currentTimeMillis();
+            catalani = catalan(i);
+            temps1 = System.currentTimeMillis() - t0;
+
+            t1 = System.currentTimeMillis();
+            catalani = catalan2(i);
+            temps2 = System.currentTimeMillis() - t1;
+
+            System.out.println("Temps de calcul du " + i + "ème nombre de Catalan :   récursif : " + temps1 + "   itératif : " + temps2);
         }
     }
 
 
-    /*
-    * @authors: Matthieu | Jeremy
-    */
+    /**
+     * Question 7 Vérifie si un mot est bien parenthésé
+     *
+     * @param mot
+     *          mot à tester
+     * @return
+     *          Vrai si le mot est bien parenthésé, faux sinon
+     */
     public static boolean verifMotBP(String mot) {
         if (mot.charAt(0) == ')') {
             System.out.println("Ce mot n'est pas bien parenthésé !\n");
@@ -72,6 +134,61 @@ public class MotBienParenthese {
         }
     }
 
+    /**
+     * Question 8 Affiche les parenthèses d'un mot avec une bonne indentation
+     *
+     * @param w mot à tester
+     */
+    public static void displayWord(String w) {
+
+        if (!verifMotBP(w)) {
+            System.out.println("Ce mot n'est pas bien parenthésé !");
+            return;
+        }
+
+        String dec = "\t";
+        String xdec = "";
+        int cpt = 0;
+        PrintStream p = System.out;
+        p.println();
+
+        for (int i = 0; i != w.length(); i++) {
+            if (w.charAt(i) == ')') {
+                xdec = repeat(dec, cpt - 1);
+                p.print(xdec);
+                cpt--;
+            } else {
+                xdec = repeat(dec, cpt);
+                p.print(xdec);
+                cpt++;
+            }
+            p.println(w.charAt(i));
+        }
+    }
+
+    /**
+     * Ajoute n espaces dans s pour afficher la bonne indentation
+     *
+     * @param s
+     *         chaîne remplie d'espaces
+     * @param n
+     *         nombre de fois qu'on va augmenter la profondeur de s
+     * @return
+     *
+     */
+    public static String repeat(String s, int n) {
+
+        if (n == 0) return "";
+        for (int i = 0; i < n; i++) s += s;
+
+        return s;
+    }
+
+    /**
+     * Question 9 Affiche la profondeur de parenthèse d'un mot
+     *
+     * @param mot Mot à tester
+     */
     public static void profondeurParenthese(String mot) {
         if (!verifMotBP(mot)) System.exit(0);
         int altitude = 0, max = 0;
@@ -83,50 +200,5 @@ public class MotBienParenthese {
         }
 
         System.out.println("Profondeur de parenthèses : " + max + '\n');
-    }
-
-    public static void displayWord(String w) {
-
-        if (verifMotBP(w)) {
-            System.out.println("Ce mot n'est pas bien parenthésé !");
-            return;
-        }
-
-        String dec = "\t";
-        String xdec = "";
-        int cpt = 0;
-
-        PrintStream p = System.out;
-        //for (int i = 0; i < w.length(); i++) {
-        //    p.print(w.charAt(i));
-        //}
-        p.println();
-
-        for (int i = 0; i != w.length(); i++) {
-            // p.print(w.charAt(i));
-            // p.print("i: "+ i + "cpt : " + cpt);
-            if (w.charAt(i) == ')') {
-
-                xdec = repeat(dec, cpt - 1);
-                p.print(xdec);
-                cpt--;
-            } else {
-
-                xdec = repeat(dec, cpt);
-                p.print(xdec);
-                cpt++;
-            }
-            p.println(w.charAt(i));
-
-        }
-    }
-
-    public static String repeat(String s, int n) {
-        //System.out.println(n);
-        if (n == 0) return "";
-        for (int i = 0; i < n; i++) {
-            s += s;
-        }
-        return s;
     }
 }
--- src/src/TestMotBienParenthese.java
+++ src/src/TestMotBienParenthese.java
@@ -1,16 +1,72 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
- * Created by user on 10/12/2014.
+ * Created by Jeremy and Matthieu on 10/12/2014.
  */
 public class TestMotBienParenthese {
 
     public static void main(String args[]) {
 
-        //MotBienParenthese.verifMotBP("()))");
-        //MotBienParenthese.profondeurParenthese("((()))");
+        boolean continu = true;
 
-        GrandEntier a = new GrandEntier();
-        a.afficher();
+        while (continu) {
+            System.out.println("Que voulez vous utiliser comme méthode ?");
+            System.out.println("enumMotsBP()           : 1");
+            System.out.println("catalan()              : 2");
+            System.out.println("catalan2()             : 3");
+            System.out.println("testCatalan2()         : 4");
+            System.out.println("verifMotsBP()          : 5");
+            System.out.println("displayWord()          : 6");
+            System.out.println("profondeurParenthese() : 7");
+            System.out.println("quitter                : 8\n");
+            try {
+                Scanner sc = new Scanner(System.in);
+                Scanner a = new Scanner(System.in);
 
-        System.out.println(MotBienParenthese.catalan(4));
+                int word = sc.nextInt();
+
+                switch (word) {
+                    case 1:
+                        System.out.println("Quelle longueur de demi mot vous voulez ?");
+                        MotBienParenthese.enumMotsBP(sc.nextInt());
+                        break;
+                    case 2:
+                        System.out.println("Quelle longueur de demi mot vous voulez ?");
+                        System.out.println(MotBienParenthese.catalan(sc.nextInt()));
+                        break;
+                    case 3:
+                        System.out.println("Quelle longueur de demi mot vous voulez ?");
+                        System.out.println(MotBienParenthese.catalan2(sc.nextInt()));
+                        break;
+                    case 4:
+                        System.out.println("Quelle longueur de demi mot vous voulez ?");
+                        MotBienParenthese.testCatalan2(sc.nextInt());
+                        break;
+                    case 5:
+                        System.out.println("Quel mot voulez-vous tester ?");
+                        MotBienParenthese.verifMotBP(a.nextLine());
+                        break;
+                    case 6:
+                        System.out.println("Quel mot voulez-vous tester ?");
+                        MotBienParenthese.displayWord(a.nextLine());
+                        break;
+                    case 7:
+                        System.out.println("Quel mot voulez-vous tester ?");
+                        MotBienParenthese.profondeurParenthese(a.nextLine());
+                        break;
+                    case 8:
+                        continu = false;
+                        break;
+                    default:
+                        System.out.println("Je n'ai pas compris votre demande");
+                        break;
+                }
+            }
+            catch (InputMismatchException e) {
+                continu = false;
+                System.out.println("Tapez un nombre !");
+            }
+        }
     }
 }
